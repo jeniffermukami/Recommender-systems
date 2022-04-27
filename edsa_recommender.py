3279b7596e549618eea5e052da9e96cab91a634e
jeniffermukami <mukamijeniffer6@gmail.com>
2022-04-27 17:46:41 +0300

    gifs

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,6 +27,8 @@
 """
 # Streamlit dependencies
 import streamlit as st
+import streamlit.components.v1 as components
+
 
 # Data handling dependencies
 import pandas as pd
@@ -39,13 +41,17 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+links = pd.read_csv('movies_link.csv')
+
 
 # App declaration
 def main():
+    st.sidebar.image("resources/imgs/tryy.gif")
+
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview","EDA","Creator"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -102,7 +108,34 @@ def main():
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+        st.write("Have you ever imagined how Amazon Prime, Netflix, and Google predict your taste in movies so easily? It is no rocket science, that after completing one movie/series you loved, and rated it on these platforms, a few more adds up to the suggested or ‘You May Like this ‘ section in seconds! It is Machine Learning. A recommendation system predicts and filters user preferences after learning about the user’s past choices. As simple as that!")
+
+
+        st.markdown("<h2 style='text-align: center; color: white;'>Content Based Filtering</h2>", unsafe_allow_html=True)
+        st.write("")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image('resources/imgs/content based filtering.png',use_column_width=True)
+        with col2:
+            st.write('')
+            st.markdown("<p style='text-align: center; color: white;'>Content-based filtering uses item features to recommend other items similar to what the user likes, based on their previous actions or explicit feedback.:</p>", unsafe_allow_html=True)
+        
+        st.markdown("<h2 style='text-align: center; color: white;'>Collaborative Filtering</h2>", unsafe_allow_html=True)
+        st.write("")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image('resources/imgs/collaborative filtering 2.png')
+        with col2:
+            st.markdown("<p style='text-align: center; color: white;'>Collaborative filtering uses algorithms to filter data from user reviews to make personalized recommendations for users with similar preferences. This is the hallmark for Recommender Systems, Giving greater insights into what users/customers are interested</p>", unsafe_allow_html=True)
+    
+    if page_selection == "Creator":
+        st.markdown("<h1 style='text-align: center; color: white;'>Creator</h1>", unsafe_allow_html=True)
+        st.image('resources/imgs/jenny2.jpeg',use_column_width=True)
+        st.markdown("<h2 style='text-align: center; color: white;'>Contact Info</h2>", unsafe_allow_html=True)
+        col = st.columns(1)
+    
+        st.markdown("<p style='text-align: center; color: white;'>Jeniffer Mariga</p>", unsafe_allow_html=True)
+        st.markdown("<p style='text-align: center; color: white;'>mukamijeniffer6@gmail.com</p>", unsafe_allow_html=True)
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
