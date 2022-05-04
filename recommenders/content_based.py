e8df4d288a6518a569829bb72a9bf4249aea0dd9
jeniffermukami <mukamijeniffer6@gmail.com>
2022-05-04 18:01:12 +0300

    Update content_based.py

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -29,38 +29,35 @@
 
 # Script dependencies
 import os
-from matplotlib.pyplot import title
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
-movies = pd.read_csv('resources/data/movies.csv')
+movies = pd.read_csv('./resources/data/movies.csv', sep = ',')
+ratings = pd.read_csv('./resources/data/ratings.csv')
 movies.dropna(inplace=True)
 
-def data_preprocessing(subset_size):
+def data_preprocessing(df):
     """Prepare data for use within Content filtering algorithm.
-
     Parameters
     ----------
     subset_size : int
         Number of movies to use within the algorithm.
-
     Returns
     -------
     Pandas Dataframe
         Subset of movies selected for content-based filtering.
-
     """
+    movies = df.copy()
     # Split genre data into individual words.
-    movies['corpus'] = movies['genres'].str.replace('|', ' ')
-    # Subset of the data
-    movies_subset = movies[:subset_size]
-    return movies_subset
+    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+    movies['genres'] = movies['genres'].apply(str).apply(lambda x: x.split('|'))
+    return movies
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
-# You are, however, encouraged to change its content.
+# You are, however, encouraged to change its content.  
 def content_model(movie_list,top_n=10):
     """Performs Content filtering based upon a list of movies supplied
        by the app user.
@@ -69,47 +66,31 @@ def content_model(movie_list,top_n=10):
     ----------
     movie_list : list (str)
         Favorite movies chosen by the app user.
-    top_n : type
-        Number of top recommendations to return to the user.
-
-    Returns
-    -------
-    list (str)
-        Titles of the top-n movie recommendations to the user.
 
     """
-    # Initializing the empty list of recommended movies
-    df_1 = data_preprocessing(27000)
-    # Instantiating and generating the count matrix
-    vect = CountVectorizer()
-    count_matrix = vect.fit_transform(df_1['corpus'])
-    title = df_1.copy()
-    title.set_index('movieId',inplace=True)
-    indices = pd.Series(title['title'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    cosine_sim = pd.DataFrame(cosine_sim, index = df_1['movieId'].values.astype(int), columns = df_1['movieId'].values.astype(int))
-    # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_2).append(score_series_3).sort_values(ascending = False)
+    #global movies
+    # removing the favorite movie list
+    nmovies = data_preprocessing(movies)
+    genre_list = []
+    for i in movie_list:
+        genre_list.append(list(nmovies[nmovies['title']==i]['genres'])[0])
 
-    listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
-    # Store movie names
-    recommended_movies = []
-    # Appending the names of movies
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies['title'])[i])
-    return recommended_movies
+
+
+    from sklearn.preprocessing import MultiLabelBinarizer
+    mlb2 =  MultiLabelBinarizer()
+    mlb2.fit_transform(genre_list)
+    genre_list = mlb2.classes_
+    nmovies = nmovies[~nmovies['title'].isin(movie_list)]
+    mgen = nmovies
+    for gen in genre_list:
+        mgen = mgen[mgen['keyWords'].str.contains(gen)]
+        if len(mgen)<=top_n:
+            break
+
+        mgen2 = mgen
+
+
+    asscr = ratings[ratings['movieId'].isin(mgen2['movieId'].values)][['movieId', 'rating']]
+    top_movies = (asscr.groupby(['movieId']).mean().reset_index()).sort_values('rating', ascending =False)[:top_n]
+    return list((nmovies[nmovies['movieId'].isin(top_movies['movieId'].values)]['title']).values)
